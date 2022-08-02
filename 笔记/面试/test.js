20c35e85661dcd17c8f867633e8e99a282e3932a
mooney666 <qwe2945@outlook.com>
2022-08-02 16:59:44 +0800

    fix

--- 笔记/面试/test.js
+++ 笔记/面试/test.js
@@ -157,14 +157,43 @@ class MyPromise {
   }
 }
 
-const p1 = new Promise((resolve, reject) => {
-  resolve(2);
-});
+// const p1 = new Promise((resolve, reject) => {
+//   resolve(2);
+// });
 
-p1.then().then((res) => console.log(res)); // 输出 2
+// p1.then().then((res) => console.log(res)); // 输出 2
 
 let a = { name: "sfs", sex: 1, desc: { n: 1, keuy: { m: 1, n: 2 } } };
 
 let b = { ...a };
 b.sex = 0;
 console.log(a, b);
+
+setTimeout(() => {
+  console.log(1);
+  let aaa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("aaa");
+      console.log(2);
+    });
+  });
+  aaa.then(() => {
+    console.log(3);
+  });
+},2);
+console.log(4);
+setTimeout(() => {
+  console.log(5);
+  let aaa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("aaa");
+      console.log(6);
+    });
+  });
+  setTimeout(() => {
+    console.log(7);
+  });
+  aaa.then(() => {
+    console.log(8);
+  });
+});
